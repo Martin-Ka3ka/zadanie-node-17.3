b594bfd11a2f386125f97ab9714ac58bdf6f4d77
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-02-17 20:27:13 +0100

    modified js/

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function () {
     
    var input = process.stdin.read();
    if (input !== null) {
-      var instruction = input.toString().trim();
       var info = process.env
-      if (instruction === '/exit') {
-         process.stdout.write('end');
+      if (info === '/node') {
+         process.stdout.write('score');
          process.exit();
       if(instruction==info==='score'){
         process.stdout.write('type of node')

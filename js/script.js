08b03fdbe48befe3c102de47944df520d5bb2853
Marcin Kaczmarek <marcin_kaczmarek@interia.eu>
2019-02-19 09:31:17 +0100

    add changes in file js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 process.stdin.setEncoding('utf-8');
+
+console.log('Node version ' + process.versions.node);
+
+
 process.stdin.on('readable', function () {
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.trim();
-        switch (instruction) {
-            case '/exit':
-                process.stdout.write('Quit program!\n')
-                process.exit();
-                break;
-            case '/info':
-                process.env.process.stdout.write('information')
-                break
-            case '/say':
-                process.stdout.write('hello!\n');
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
-    }
+   var input = process.stdin.read();
+   if (input !== null) {
+      var instruction = input.trim();
+
+      switch (instruction) {
+         case '/exit':
+            process.stdout.write('Quit program!\n')
+            process.exit();
+            break;
+         case '/sayhello':
+            process.stdout.write('hello!\n');
+            break;
+         default:
+            process.stderr.write('Wrong instruction!\n');
+      };
+   }
 });
